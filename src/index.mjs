ed18a4e5c5c13dc617ca7e3a032d03d40deac4fa
Wuwuh7 <222758903+Wuwuh7@users.noreply.github.com>
2026-08-14 16:52:49 +0000

    migrate the midleware auth to single file midleware

--- src/index.mjs
+++ src/index.mjs
@@ -1,7 +1,6 @@
 import express from "express";
 import  route  from "./routes.mjs";
-import { userDb } from "./service.mjs";
-import expressBasicAuth from "express-basic-auth";
+
 
 const app = express();
 const port = 8080;
@@ -10,21 +9,6 @@ app.use("/", (req, res, next) => {
     next();
 });
 
-async function checkAuth(req,res,next) {
-    const authentication = expressBasicAuth(req);
-    const { email,password } = userDb;
-
-    if (!authentication.email && authentication.email !== email && !authentication.password && authentication.password !== password) {
-        res.set('WWW-Authenticate', 'Basic realm ="example"');
-        return res.status(401).send('Unauthorized');
-    } else {
-        next();
-}
-}
-
-app.get("/auth",checkAuth, (req,res) => {
-    res.send("MONGGOH MASSEH");
-})
 app.use("/", route);
 
 
